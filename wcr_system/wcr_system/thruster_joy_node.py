7d2428195e3791f2885ce34a711582f5b1873a1d
BCaran <branimir.caran@gmail.com>
2025-05-08 16:32:07 +0200

    changed /thruster_pwm to /wcr/thruster_pwm

--- wcr_system/wcr_system/thruster_joy_node.py
+++ wcr_system/wcr_system/thruster_joy_node.py
@@ -12,7 +12,7 @@ class TrhusterJoyNode(Node):
         self.joy_subscribtion = self.create_subscription(Joy, 'joy', self.joy_callback, 10)
         self.joy_subscribtion 
 
-        self.thr_publihser = self.create_publisher(Int8, 'thruster_pwm', 10)
+        self.thr_publihser = self.create_publisher(Int8, '/wcr/thruster_pwm', 10)
         self.edf_publisher = self.create_publisher(Float64, 'commands/motor/duty_cycle', 10)
 
         self.thruster_pwm = Int8()
